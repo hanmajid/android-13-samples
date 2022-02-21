e17fddb2a99e8ef8f2ec7f59270ef9ff3c280526
hanmajid <han.majid2004@gmail.com>
2022-02-21 11:48:43 +0700

    Initial commit

---
+++ app/src/main/java/com/hanmajid/android/tiramisu/themedappicon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanmajid.android.tiramisu.themedappicon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
