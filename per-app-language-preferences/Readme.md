5ad2b6cb604c8add1be79d985dce8fa10a0ec8d1
hanmajid <han.majid2004@gmail.com>
2022-02-22 09:20:20 +0700

    Add per-app-language-preferences project

---
+++ per-app-language-preferences/Readme.md
@@ -0,0 +1,7 @@
+# per-app-language-preferences
+
+<div align="center">
+    <img src="screenshots/android-13-per-app-language-pref.png" />
+</div>
+
+This application demonstrates Android 13's new feature: [Per-App Language Preferences](https://developer.android.com/about/versions/13/features/app-languages).
---
+++ per-app-language-preferences/app/src/main/java/com/hanmajid/android/tiramisu/perapplanguagepreferences/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.hanmajid.android.tiramisu.perapplanguagepreferences
+
+import android.annotation.SuppressLint
+import android.app.LocaleManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.os.LocaleList
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+@SuppressLint("InlinedApi")
+class MainActivity : AppCompatActivity() {
+    private var localeManager: LocaleManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.CODENAME == "Tiramisu") {
+            localeManager =
+                getSystemService(Context.LOCALE_SERVICE) as LocaleManager
+        }
+
+        findViewById<Button>(R.id.button_lang_en).setOnClickListener {
+            localeManager?.applicationLocales = LocaleList(Locale.forLanguageTag("en"))
+        }
+        findViewById<Button>(R.id.button_lang_id).setOnClickListener {
+            localeManager?.applicationLocales = LocaleList(Locale.forLanguageTag("id-ID"))
+        }
+        findViewById<Button>(R.id.button_lang_reset).setOnClickListener {
+            localeManager?.applicationLocales = LocaleList.getEmptyLocaleList()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (Build.VERSION.CODENAME == "Tiramisu") {
+            val language = when (localeManager?.applicationLocales?.toLanguageTags()) {
+                "en" -> "English"
+                "id-ID" -> "Indonesian"
+                else -> "Not Set"
+            }
+            Log.wtf("LANG", localeManager?.applicationLocales?.toLanguageTags())
+            findViewById<TextView>(R.id.text_current_language).text =
+                "Current In-App Language: $language"
+        }
+    }
+}
