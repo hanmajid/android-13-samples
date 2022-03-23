22d4ca628f0c43575ed7fc14bc28d61938e9a71b
hanmajid <han.majid2004@gmail.com>
2022-03-23 15:24:26 +0700

    Finish notification-runtime-permission project

---
+++ notification-runtime-permission/Readme.md
@@ -0,0 +1,7 @@
+# notification-runtime-permission
+
+<div align="center">
+    <img src="screenshots/android-13-notification-runtime-permission.png" />
+</div>
+
+This application demonstrates Android 13's new feature: [Notification Runtime Permission](https://developer.android.com/about/versions/13/changes/notification-permission).
--- notification-runtime-permission/app/src/main/java/com/hanmajid/android/tiramisu/notificationruntimepermission/MainActivity.kt
+++ notification-runtime-permission/app/src/main/java/com/hanmajid/android/tiramisu/notificationruntimepermission/MainActivity.kt
@@ -1,11 +1,110 @@
 package com.hanmajid.android.tiramisu.notificationruntimepermission
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private val notificationManager: NotificationManager by lazy {
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+    @RequiresApi(33)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Sets up permissions request launcher.
+        requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
+            refreshUI()
+            if (it) {
+                showDummyNotification()
+            } else {
+                Snackbar.make(
+                    findViewById<View>(android.R.id.content).rootView,
+                    "Please grant Notification permission from App Settings",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        // Sets up notification channel.
+        createNotificationChannel()
+
+        // Sets up button.
+        findViewById<Button>(R.id.button_show_notification).setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS,
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showDummyNotification()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+
+        // Refresh UI.
+        refreshUI()
+    }
+
+    /**
+     * Refresh UI elements.
+     */
+    private fun refreshUI() {
+        findViewById<TextView>(R.id.text_notification_enabled).text =
+            if (notificationManager.areNotificationsEnabled()) "TRUE" else "FALSE"
+    }
+
+    /**
+     * Creates Notification Channel (required for API level >= 26) before sending any notification.
+     */
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            "Important Notification Channel",
+            NotificationManager.IMPORTANCE_HIGH,
+        ).apply {
+            description = "This notification contains important announcement, etc."
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    /**
+     * Shows a notification to user.
+     *
+     * The notification won't appear if the user doesn't grant notification permission first.
+     */
+    private fun showDummyNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Congratulations! 🎉🎉🎉")
+            .setContentText("You have post a notification to Android 13!!!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, builder.build())
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "dummy_channel"
     }
 }
