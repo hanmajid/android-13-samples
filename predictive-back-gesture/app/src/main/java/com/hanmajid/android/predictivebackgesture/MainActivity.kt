b4fe9611184fc0d658715f3d6512b6262bab06a3
hanmajid <han.majid2004@gmail.com>
2022-07-11 14:21:58 +0700

    Finish predictive-back-gesture project

--- predictive-back-gesture/app/src/main/java/com/hanmajid/android/predictivebackgesture/MainActivity.kt
+++ predictive-back-gesture/app/src/main/java/com/hanmajid/android/predictivebackgesture/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.hanmajid.android.predictivebackgesture
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.window.OnBackInvokedCallback
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var onBackInvokedCallback: OnBackInvokedCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // For Non-AndroidX:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            onBackInvokedCallback = OnBackInvokedCallback {
+                // Your app's onBackPressed logic goes here...
+            }
+            onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                OnBackInvokedDispatcher.PRIORITY_DEFAULT,
+                onBackInvokedCallback,
+            )
+        }
+
+        // For AndroidX:
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Your app's onBackPressed logic goes here...
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+        // You can enable/disable onBackPressedCallback by using `isEnabled` method:
+        // onBackPressedCallback.isEnabled = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // For Non-AndroidX:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            onBackInvokedDispatcher.unregisterOnBackInvokedCallback(onBackInvokedCallback)
+        }
     }
 }
