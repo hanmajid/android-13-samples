35be3df8ae88816c0c0e5ee9fb41bdfe68206105
hanmajid <han.majid2004@gmail.com>
2022-07-11 10:28:50 +0700

    Init predictive-back-gesture project

---
+++ predictive-back-gesture/app/src/main/java/com/hanmajid/android/predictivebackgesture/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanmajid.android.predictivebackgesture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
